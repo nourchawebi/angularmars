2181334f9620b1a515f80279d2bc9522e4b6e607
nourchawebi <nourelhouda.chawebi@esprit.tn>
2025-03-05 22:13:00 +0100

    first push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angualarsmars';
+  title:string = 'angualarsmars';
+  buttonClass:string ='red';
+  toggleColor(){
+    this.buttonClass=this.buttonClass==='red'?'blue': 'red';
+    if(this.buttonClass==='red'){
+      this.buttonClass='blue'
+    }
+    else{
+      this.buttonClass='red'
+    }
+  }
 }
