3cb01868b2e32e1fee64b0130918864d279d76b3
nourchawebi <nourelhouda.chawebi@esprit.tn>
2025-03-08 09:36:12 +0100

    2nd push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,18 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+
 })
 export class AppComponent {
   title:string = 'angualarsmars';
   buttonClass:string ='red';
+
   toggleColor(){
     this.buttonClass=this.buttonClass==='red'?'blue': 'red';
-    if(this.buttonClass==='red'){
-      this.buttonClass='blue'
-    }
-    else{
-      this.buttonClass='red'
-    }
+
   }
 }
